117ec39948db5c0a0718262b2de073e7a3759407
BlastKami <thegamergod12@gmail.com>
2018-03-21 15:49:56 -0500

    random

---
+++ js/config.js
@@ -0,0 +1,184 @@
+﻿/*    _           _    _                     _ 
+     | |         | |  | |                   | |
+  ___| | ___  ___| | _| |     ___   __ _  __| |
+ / __| |/ _ \/ _ \ |/ / |    / _ \ / _` |/ _` |
+ \__ \ |  __/  __/   <| |___| (_) | (_| | (_| |
+ |___/_|\___|\___|_|\_\______\___/ \__,_|\__,_|
+    version 2.10
+*/
+
+/*URL for the server logo
+Leave it blank to use text instead
+(Put the files in the images folder)*/
+var l_serverImage = "logo.png";
+
+/*Display current map and gamemode?
+'true' or 'false'*/
+var l_displayMapGamemode = true;
+
+/*Custom server name configuration
+Leave it blank for automatic server name*/
+var l_serverName = "";
+
+/*Use a video for the background?
+Otherwise it will use images
+'true' or 'false'*/
+var l_bgVideo = false;
+
+/*Default available videos:
+"aurora.webm"
+"bluefall.webm"
+"bluetilefloor.webm"
+"bluewhitecircles.webm"
+"bokehlens.webm"
+"cleanbokeh.webm"
+"coldnight.webm"
+"colorfulburst.webm"
+"dancingstars.webm"
+"dropsflares.webm"
+"focusorangebokeh.webm"
+"glimmering.webm"
+"glowingrings.webm"
+"greencircles.webm"
+"hues.webm"
+"inthedeep.webm"
+"modernflourish.webm"
+"orangeaura.webm"
+"warminside.webm"
+"yellowgreen.webm"
+
+You can add your own video backgrounds
+Just make sure they are using webm VP8 format 
+(Place the videos in the backgrounds/videos folder)*/
+var l_background = "bluefall.webm";
+
+/*(Image-background only)
+Enable map-based background image?
+If enabled, the background will be an image based on the server current map
+You need to have a background image with the same name as the map
+Also, the image needs to be in .jpg format
+E.g: gm_flatgrass.jpg, gm_construct.jpg, rp_downtown_v4c_v2.jpg
+'true' or 'false'*/
+var l_bgImageMapBased = false;
+
+/*(Image-background only)
+Place one or more images
+Multiple images will automatically shuffle between them
+(Place the files in the backgrounds/images folder)*/
+var l_bgImages = [
+	"w1.jpg",
+	"w2.jpg",
+	"w3.jpg",
+	"w4.jpg",
+	"w5.jpg",
+	"w6.jpg",
+	"w7.jpg",
+	"w8.jpg",
+	"w9.jpg",
+	"w10.jpg",
+	"w11.jpg",
+	"w12.jpg",
+	"w12.jpg",
+	"w13.jpg",
+	"w14.jpg",
+	"w15.jpg",
+	"w16.jpg",
+	"w17.jpg",
+	"w18.jpg",
+	"w19.jpg",
+	"w20.jpg",
+	"w21.jpg",
+	"w22.jpg",
+	"w23.jpg",
+	"w24.jpg",
+	"w25.jpg",
+	"w26.jpg",
+	"w27.jpg",
+	"w28.jpg",
+	"w29.jpg",
+	"w30.jpg",
+	"w31.jpg",
+];
+
+/*(Image-background only)
+Random background images order?
+'true' or 'false'*/
+var l_bgImagesRandom = true;
+
+/*(Image-background only)
+Delay between background images changes
+in milliseconds*/
+var l_bgImageDuration = 5000;
+
+/*(Image-background only)
+Image background fade velocity
+in milliseconds*/
+var l_bgImageFadeVelocity = 2000;
+
+/*Enable background overlay?
+'true' or 'false'*/
+var l_bgOverlay = true;
+
+/*Background darkening amount
+0 for none (0%), 100 for pitch black (100%)*/
+var l_bgDarkening = 50;
+
+/*Play music during the loading screen?
+'true' or 'false'*/
+var l_music = false;
+
+/*Display current music name?
+'true' or 'false'*/
+var l_musicDisplay = false;
+
+/*Music playlist
+Add as many youtube ids or ogg files you want
+(Place the ogg files in the music folder)*/
+var l_musicPlaylist = [
+	{ogg: "lullaby.ogg", name: "Lullaby of Deserted Hell"},
+	{ogg: "falls_in_fall.ogg", name: "秋に舞う椛と滝飛沫"},
+	{youtube: "zA6wRutN_7Q", name: "Suwa-cafeteria"},
+	{youtube: "KtlKbE4qU0c", name: "月の妖鳥、化猫の幻"},
+];
+
+/*Random music order?
+'true' or 'false'*/
+var l_musicRandom = true;
+
+/*Music volume
+Choose a value between 0 (quietest) and 100 (loudest)*/
+var l_musicVolume = 0;
+
+/*Enable custom messages?
+'true' or 'false'*/
+var l_messagesEnabled = true;
+
+/*Enter your custom messages below*/
+var l_messages = [
+	"Enjoy the server",
+	"Thanks for joining us!",
+	"Check out the rules and forums!",
+	"I hope you like the server and continue to play on it!-Blast",
+
+];
+
+/*Random message order?
+'true' or 'false'*/
+var l_messagesRandom = true;
+
+/*Delay between message changes
+in milliseconds*/
+var l_messagesDelay = 5000;
+
+/*Messages fade time
+in milliseconds*/
+var l_messagesFade = 1000;
+
+//==============================================================
+//========================== WARNING ===========================
+//==============================================================
+//============ Do not edit anything below this line ============
+//==============================================================
+var checkConfigFile = function() {
+	return true;
+};
